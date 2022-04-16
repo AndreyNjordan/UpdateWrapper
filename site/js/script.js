ff4f0569eaa998e7a494e42adbc737fb599781dc
Njordan <novozhilov0128@gmail.com>
2022-04-16 17:25:28 +0300

    start bootstrap view

--- site/js/script.js
+++ site/js/script.js
@@ -1,9 +1,22 @@
-console.log("Hello Njordan!");
+console.log("UpdateWrapper running!");
 
-function getJSOriginal(){
-    var req = new XMLHttpRequest();
-    req.open("POST", "/jsondata", false);
+function TestFunc () {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open("POST", "/jsondata", false);
+        req.send(null);
+        resolve(req.responseText);
+    })
+}
+
+function LogResponse () {
+    console.log(this.responseText);
+    console.log(this.response);
+}
+
+function GetPluginUpdateContent () {
+    const req = new XMLHttpRequest();
+    req.open("GET", "http://update.sbis.ru/Sbis3Plugin", true)
     req.send(null);
-    console.log(JSON.parse(req.responseText));
-    // window.location.href = "/site/js/script.js";
+    req.onload = LogResponse;
 }
